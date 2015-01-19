dcd794704d4b0dc0aa8dfc0465cef02df6f924c6
Yuan Chen <yuan@joycastle.mobi>
2015-01-19 17:04:22 +0800

    parse  expression

--- LuaAutoComplete.py
+++ LuaAutoComplete.py
@@ -100,6 +100,14 @@ def push_value(obj, key, value):
     else:
         obj[key] = [value]
 
+def get_project_path():
+    window = sublime.active_window()
+    if window:
+        folders = window.folders()
+        if folders:
+            return folders[0]
+    return None
+
 
 def parse_assignment_statement(filename, ast_obj):
     if ast_obj["type"] == "AssignmentStatement":
@@ -110,34 +118,97 @@ def parse_assignment_statement(filename, ast_obj):
                 global FILE_RELATED
                 push_value( FILE_RELATED, filename, v["name"])
 
+def parse_require_statement(filename, ast_obj):
+    if ast_obj["type"] == "CallExpression" and \
+        ast_obj["base"]["type"] == "Identifier" and \
+        ast_obj["base"]["name"] == "require":
+        if ast_obj["arguments"] and \
+            ast_obj["arguments"][0]["type"] == "StringLiteral":
+
+            required_module = ast_obj["arguments"][0]["value"]
+            required_module = required_module.replace(r".", "/") + ".lua"
+
+            project_path = get_project_path()
+
+            if project_path:
+                global settings
+                for module_path in settings.lua_package_paths:
+                    module_path = os.path.join(project_path, module_path, required_module)
+                    if os.path.exists(module_path):
+                        parse(module_path)
+                        return;
+
 
 def iterate_ast(filename, ast_obj):
     if ast_obj["type"] in ["Chunk"]:
         for b in ast_obj["body"]:
             iterate_ast(filename, b)
-    else:
+    elif ast_obj["type"] == "AssignmentStatement":
         parse_assignment_statement(filename, ast_obj)
+        for c in ast_obj["init"]:
+            iterate_ast(filename, c)
+    elif ast_obj["type"] == "CallExpression":
+        iterate_ast(filename, ast_obj["base"])
+        parse_require_statement(filename, ast_obj)
+    elif ast_obj["type"] == "CallStatement":
+        iterate_ast(filename, ast_obj["expression"])
+    elif ast_obj["type"] == "MemberExpression":
+        iterate_ast(filename, ast_obj["base"])
 
 def parse(lua_src_file):
+    global FILE_RELATED
+    if lua_src_file in FILE_RELATED:
+        return
+
     raw = get_raw_ast(lua_src_file)
-    for ast_obj in iterload(raw):
-        iterate_ast(lua_src_file, ast_obj)
+    if raw:
+        for ast_obj in iterload(raw):
+            iterate_ast(lua_src_file, ast_obj)
+    else:
+        print("Get AST from lua files error.")
 
 class LuaReloadCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        file_start = self.view.settings().get("lua_entry_file", self.view.file_name())
-        print("Reload ", file_start)
-        if file_start and file_start.endswith(".lua"):
-            parse(file_start)
+        project_lua_package_paths = self.view.settings().get("lua_package_paths")
+        if project_lua_package_paths and isinstance(project_lua_package_paths, list):
+            global settings
+            settings.lua_package_paths.extend(project_lua_package_paths)
+
+        file_start = self.view.settings().get("lua_entry_file", None)
+        if not file_start:
+            print("Please set `lua_entry_file` to reload lua.")
+            return
+
+        file_start_path = ""
+        if file_start.startswith("/"):
+            file_start_path = file_start
+        else:
+            project_path = get_project_path()
+            if project_path:
+                file_start_path = os.path.join(project_path, file_start)
+
+        if os.path.exists(file_start_path) and file_start_path.endswith(".lua"):
+            global VARIABLES
+            global FUNCTIONS
+            global FILE_RELATED
+            VARIABLES = {}
+            FUNCTIONS = {}
+            FILE_RELATED = {}
+            print("Reload: ", file_start_path)
+            parse(file_start_path)
+
+        print("VARIABLES:", VARIABLES.keys())
+        print("FUNCTIONS:", FUNCTIONS)
+        print("FILES:", FILE_RELATED.keys())
 
 class LuaAutocomplete(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
         if view.match_selector(locations[0], "source.lua"):
             results = []
+            global VARIABLES
             for key in VARIABLES.keys():
-                if key.startswith(prefix):
+                if key.upper().startswith(prefix.upper()):
                     results.append(key)
             if len(results) > 0:
                 # return list(set(results))
-                return (list(set(results)),
-                        sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+                return (list(set(results)), sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
